21fcc5c36db9ed28085ea5102bc6b541c9eeacde
Devanand Thirunavukkarasu <D.Thirunavukkarasu@perficient.com>
2022-07-29 11:56:01 +0530

    array file 5th array declaration done

--- Arrayexample.java
+++ Arrayexample.java
@@ -15,6 +15,8 @@ public class Arrayexample {
 Map<String,Integer> mymap2 = new HashMap<String,Integer>();
 Map<String,Integer> mymap3 = new HashMap<String,Integer>();
 Map<String,Integer> mymap4 = new HashMap<String,Integer>();
+Map<String,Integer> mymap5 = new HashMap<String,Integer>();
+
 
 String[] name = {"deva","kal","veda","karan","deva","kal","daran","darshan","fer","gh"};
 //for(int i =0; i<= name.length-1;i++)
